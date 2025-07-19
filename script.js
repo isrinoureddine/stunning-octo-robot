f69e3e98e6d29777fc1343a04b990abc28c98ee9
isrinoureddine <isrinounou2006@gmail.com>
2025-07-19 12:15:16 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const switcher = document.getElementById('languageSwitcher');
-const allLangElements = document.querySelectorAll('[data-lang]');
-
-switcher.addEventListener('change', (e) => {
-  const selected = e.target.value;
-  allLangElements.forEach(el => {
-    el.style.display = el.getAttribute('data-lang') === selected ? '' : 'none';
-  });
-});
